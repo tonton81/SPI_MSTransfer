0db7ff3ab011588ea95acddde9fa8468fb648d08
Antonio Brewer <yodabox64@hotmail.com>
2018-04-22 12:58:56 -0400

    Update circular_buffer.h

--- circular_buffer.h
+++ circular_buffer.h
@@ -36,7 +36,6 @@
 #ifndef CIRCULAR_BUFFER_H
 #define CIRCULAR_BUFFER_H
 #include <algorithm>
-#include <typeinfo> 
 
 template<typename T, uint16_t _size, uint16_t multi = 0>
 class Circular_Buffer {
